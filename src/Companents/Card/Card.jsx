a44c0041fe49412eaed2dd3755d111c1959a7fb1
Azimnarimanov2003 <161734407+Azimnarimanov2003@users.noreply.github.com>
2024-07-26 14:48:23 +0500

    delete

--- src/Companents/Card/Card.jsx
+++ src/Companents/Card/Card.jsx
@@ -1,40 +1,40 @@
 import React from 'react';
 import './Card.css';
 import useShareStore from '../Store/Store';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Card = ({ data }) => {
-  // useShareStore Hook-ini komponent ichida chaqiring
   const { cards, setCards } = useShareStore();
+
+  const notify = () => toast("Wow so easy!");
 
   const handleAdd = (item) => {
     setCards([...cards, item]);
+    notify();
   };
 
   if (!data) {
     return <div></div>;
   }
 
-  return (
-    <>
-      <div className="card">
-        <div className="container">
-          <div className="card-box">
-            <h3 className="card-name">
-              {data.title}
 
-            
-            
-            </h3>
-            <img src={data.img} alt="photo" className="card-img" />
-            <p className="card-text">{data.text}</p>
-            <li className="card-item">
-              <p className="card-money">{data.price}$</p>
-              <button className="card-button" onClick={() => handleAdd(data)}> Enter</button>
-            </li>
-          </div>
+  
+  return (
+    <div className="card">
+      <div className="container">
+        <ToastContainer />
+        <div className="card-box">
+          <h3 className="card-name">{data.title}</h3>
+          <img src={data.img} alt="photo" className="card-img" /> 
+          <p className="card-text">{data.text}</p>
+          <li className="card-item">
+            <p className="card-money">{data.price}$</p>
+            <button className="card-button" onClick={() => handleAdd(data)}>Enter</button>
+          </li>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
--- src/Companents/Korzinka/Korzinka.jsx
+++ src/Companents/Korzinka/Korzinka.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from 'react';
+import useShareStore from '../Store/Store';
+import './Korzinka.css';
 
-export default function () {
+export default function Korzinka() {
+  const { cards, setCards } = useShareStore();
+
+  const deleteCards = (id) => {
+    const updatedCards = cards.filter(item => item.id !== id);
+    setCards(updatedCards);
+  };
+
   return (
-    <div   className='korzinka  container'>
-        Korzinka 
+    <div className='korzinka container'>
+      <h1> "Foods" sizning buyurtmalaringiz jadvali </h1>
+      {cards && cards.map((item, index) => (
+        <div className="card" key={index}>
+          <h1>{item.title}</h1>
+          <img className='korzinka-img' src={item.img} alt="rasm" />
+          <h3>{item.price}</h3>
+          <button className="card-btn" onClick={() => deleteCards(item.id)}>delete</button>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
